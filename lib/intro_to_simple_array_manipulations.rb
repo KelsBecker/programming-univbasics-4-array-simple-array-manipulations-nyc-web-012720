a54fc5880d59dd1a4ae15b8ce191cfa2cbe42154
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-07 22:31:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,8 +27,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
   
- def using_insert(array, item)
-   array.insert(2, item)
+ def using_insert(array, element)
+   array.insert(4, item)
 end
   
   
